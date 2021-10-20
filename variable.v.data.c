f63a485cc5b6b8b1d8ca4d3ba6162de10edcccf9
dasarividya <92290909+dasarividya@users.noreply.github.com>
2021-10-20 12:20:15 +0530

    Add files via upload

---
+++ variable.v.data.c
@@ -0,0 +1,19 @@
+/* Print data of the student */
+#include<stdio.h>
+void main()
+{
+	// Variable Declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	// Initialization
+	age = 19;
+	cgpa = 8.5;
+	initial = 'D';
+	
+	// Output
+	printf("student age is: %i\n", age);
+	printf("cgpa of the student is: %f\n", cgpa);
+	printf("initial of the student is : %c\n", initial);
+}
