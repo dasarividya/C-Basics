5326a4ea34eafbdc351c0f9724bedff4ad9c266a
dasarividya <92290909+dasarividya@users.noreply.github.com>
2021-11-02 12:24:16 +0530

    Add files via upload

---
+++ Natural numbers sum.c
@@ -0,0 +1,16 @@
+/* Natural Numbers */
+#include<stdio.h>
+void main()
+{
+	// Variable Declaration
+	int n, r;
+	
+	// Initialization
+	n= 20;
+	
+	// Calculation
+	r= n*(n+1)/2;
+	
+	// Output
+	printf("n= 20\nSum of first %d natural numbers is: %d", n, r);
+}
