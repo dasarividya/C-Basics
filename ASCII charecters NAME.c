bd6e76ed26eabc9da6dd8f36c7bd9591459c5fee
dasarividya <92290909+dasarividya@users.noreply.github.com>
2021-11-15 11:54:46 +0530

    Add files via upload

---
+++ ASCII charecters NAME.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d,e,f;
+	printf("Enter six numbers:");
+	scanf("%d,%d,%d,%d,%d,%d", &a,&b,&c,&d,&e,&f);
+	printf("Corresponding ASCII characters for %d, %d, %d, %d, %d, %d is: %c, %c, %c, %c, %c, %c", a,b,c,d,e,f, a,b,c,d,e,f);
+	
+}
+
+
